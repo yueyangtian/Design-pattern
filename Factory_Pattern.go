892785511a710c540e931558aa4af2a4a51d1219
田越洋 <tyy@tianyueyangdeMacBook-Pro.local>
2020-06-08 23:36:50 +0800

    add factory pattern

---
+++ Factory_Pattern.go
@@ -0,0 +1,40 @@
+package DesginPattern
+
+import (
+	"fmt"
+	"errors"
+)
+type Shape interface {
+    Draw() string
+}
+
+type Circle struct {}
+type Square struct {}
+type Rectangle struct {}
+
+func (* Circle) Draw() string {
+	return "Cricle"
+}
+
+func (* Square) Draw() string {
+	return "Square"
+}
+
+func (* Rectangle) Draw() string {
+	return "Rectangle"
+}
+
+func Create(shape string) (Shape, error) {
+	switch shape {
+	case "Cricle":
+		return new(Circle), nil
+	case "Square":
+		return new(Square), nil
+	case "Rectangle":
+		return new(Square), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("%s not in Shape", shape))
+		
+	}
+}
+
