62682bb467469248e67168862383e2b150a6a14b
田越洋 <tyy@tianyueyangdeMacBook-Pro.local>
2020-06-09 21:00:13 +0800

    add Abstract Factory Pattern

---
+++ Abstract_Factory_Pattern.go
@@ -0,0 +1,34 @@
+package DesginPattern
+
+type Worker interface {
+	Work() string
+}
+type WorkerCreate interface {
+	Create() Worker
+}
+
+//doctor
+type Doctor struct{}
+
+func (*Doctor) Work() string {
+	return "Doctor do something"
+}
+
+type DoctorCreator struct{}
+
+func (*DoctorCreator) Create() Worker {
+	return new(Doctor)
+}
+
+//programer
+type Programer struct{}
+
+func (*Programer) Work() string {
+	return "Programer do something"
+}
+
+type ProgramerCreator struct{}
+
+func (*ProgramerCreator) Create() Worker {
+	return new(Programer)
+}
---
+++ Abstract_Factory_Pattern_test.go
@@ -0,0 +1,19 @@
+package DesginPattern
+
+import (
+	"testing"
+)
+
+func TestDoctor(t *testing.T) {
+	var c WorkerCreate
+	c = new(DoctorCreator)
+	p := c.Create()
+	t.Log(p.Work())
+}
+
+func TestProgramer(t *testing.T) {
+	var c WorkerCreate
+	c = new(ProgramerCreator)
+	p := c.Create()
+	t.Log(p.Work())
+}
