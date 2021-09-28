d25146b8efb35f6e4a863df13031e47b63cfbd5a
田越洋 <tianyueyang@C02D5617ML7H.local>
2021-09-28 20:08:57 +0800

    add abstract factory

--- Abstract_Factory_Pattern/go/Abstract_Factory_Pattern.go
+++ Abstract_Factory_Pattern/go/Abstract_Factory_Pattern.go
@@ -1,34 +1,56 @@
 package Abstract_Factory_Pattern
 
-type Worker interface {
-	Work() string
-}
-type WorkerCreate interface {
-	Create() Worker
+type Shape interface {
+	Draw() string
 }
 
-//doctor
-type Doctor struct{}
+type Circle struct{}
+type Square struct{}
+type Rectangle struct{}
 
-func (*Doctor) Work() string {
-	return "Doctor do something"
+func (*Circle) Draw() string {
+	return "Cricle"
 }
 
-type DoctorCreator struct{}
-
-func (*DoctorCreator) Create() Worker {
-	return new(Doctor)
+func (*Square) Draw() string {
+	return "Square"
 }
 
-//programer
-type Programer struct{}
-
-func (*Programer) Work() string {
-	return "Programer do something"
+func (*Rectangle) Draw() string {
+	return "Rectangle"
 }
 
-type ProgramerCreator struct{}
+type Color interface {
+	Tint() string
+}
+type Red struct{}
+type Black struct{}
 
-func (*ProgramerCreator) Create() Worker {
-	return new(Programer)
+func (*Red) Tint() string {
+	return "Red"
+}
+func (*Black) Tint() string {
+	return "Black"
+}
+
+type Factory interface {
+	CreateShape() Shape
+	CreateColor() Color
+}
+
+type RedCircleFactory struct{}
+type BlackRectangleFactory struct{}
+
+func (*RedCircleFactory) CreateShape() Shape {
+	return &Circle{}
+}
+func (*RedCircleFactory) CreateColor() Color {
+	return &Red{}
+}
+
+func (*BlackRectangleFactory) CreateShape() Shape {
+	return &Rectangle{}
+}
+func (*BlackRectangleFactory) CreateColor() Color {
+	return &Black{}
 }
--- Abstract_Factory_Pattern/go/Abstract_Factory_Pattern_test.go
+++ Abstract_Factory_Pattern/go/Abstract_Factory_Pattern_test.go
@@ -4,16 +4,11 @@ import (
 	"testing"
 )
 
-func TestDoctor(t *testing.T) {
-	var c WorkerCreate
-	c = new(DoctorCreator)
-	p := c.Create()
-	t.Log(p.Work())
-}
-
-func TestProgramer(t *testing.T) {
-	var c WorkerCreate
-	c = new(ProgramerCreator)
-	p := c.Create()
-	t.Log(p.Work())
+func TestAbstract(t *testing.T) {
+	var f1 Factory
+	f1 = new(BlackRectangleFactory)
+	s1 := f1.CreateShape()
+	c1 := f1.CreateColor()
+	t.Log(s1.Draw())
+	t.Log(c1.Tint())
 }
